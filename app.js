21886bf3dcdbd268de9040a8cbe16f1c0460a118
Habil Omune <habilhm@gmail.com>
2016-11-23 07:04:49 +0300

    defult product sectiion
    defult product sectiion

--- app.js
+++ app.js
@@ -10,17 +10,6 @@ $(document).ready(function () {
     });
 
 
-
-            $('#fullpage').fullpage({
-                menu: '#menu',
-                anchors: ['home', 'about', 'featured-products','Outdoor Safety','First-Aid','Camping Equipment','Meals Ready To Eat','Other Products', 'checklist', 'clients','contact','try'],
-                sectionsColor: ['#333399', '#f8f8f8', '#e8e8e8', '#d9d9d9', '#e8e8e8', '#242222','#333399','#d9d9d9', '#e8e8e8', '#e8e8e8','#333399'],
-                navigation: false,
-                scrollingSpeed: 1500,
-                scrollOverflow: true,
-            });
-
-
     $('.sli').slick({
         dots: true,
         infinate: true,
--- controller.js
+++ controller.js
@@ -4,33 +4,45 @@ var survivalequip = angular.module('survivalequip', ['ui.bootstrap']);
 
 survivalequip.controller('productsCtrl', function ($scope, $http, $rootScope, $timeout) {
 
+
+    $http
+        ({
+            method: 'GET',
+            url: 'https://survival-eqip-limited.firebaseio.com/products/Meals Ready To Eat.json',
+        })
+        .then(function (res) {
+        $rootScope.selectedProduct = res.data;
+        })
+
+
     $scope.setPrd = function (value) {
         $rootScope.selectedProduct = value;
+        console.log(value);
+
     }
 
-        $scope.setSubPrd = function (product) {
+    $scope.setSubPrd = function (product) {
         $scope.desc = true;
         $rootScope.selectedSubProduct = product;
     }
 
 
 
-$http
-({
-    method: 'GET',
-    url: 'https://survival-eqip-limited.firebaseio.com/products.json',
-})
-.then(function(res){
- $scope.products = res.data;
-})
-.catch(function(err){
-console.log(err);
-$scope.products = {};
-})
+    $http
+        ({
+            method: 'GET',
+            url: 'https://survival-eqip-limited.firebaseio.com/products.json',
+        })
+        .then(function (res) {
+            $scope.products = res.data;
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
 
 
 
-   
+
 
     $scope.populate = function (value, key) {
         $rootScope.detail = value;
