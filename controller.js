f2d3568370ed25d33daff6409080b38ee7f92e2f
Habil Omune <habilhm@gmail.com>
2016-11-28 16:04:08 +0300

    pre release

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 
 
-var survivalequip = angular.module('survivalequip', ['ui.bootstrap','firebase']);
+var survivalequip = angular.module('survivalequip', ['ui.bootstrap', 'firebase']);
 
-survivalequip.controller('productsCtrl', function ($scope,$firebaseArray,$firebaseObject, $http, $rootScope, $timeout) {
+survivalequip.controller('productsCtrl', function ($scope, $firebaseArray, $firebaseObject, $http, $rootScope, $timeout) {
     var config = {
         apiKey: "AIzaSyA5DHBONu4Zza7w-ep7uznIXYpRCg6oCzQ",
         authDomain: "survival-eqip-limited.firebaseapp.com",
@@ -11,23 +11,24 @@ survivalequip.controller('productsCtrl', function ($scope,$firebaseArray,$fireba
         messagingSenderId: "115364664381"
     };
     firebase.initializeApp(config);
-$scope.getDefult = function(){
+    $scope.getDefult = function () {
 
-    $http
-        ({
-            method: 'GET',
-            url: 'https://survival-eqip-limited.firebaseio.com/products/Meals Ready To Eat.json',
-        })
-        .then(function (res) {
-        $rootScope.selectedProduct = res.data;
-        })
-}
+        $http
+            ({
+                method: 'GET',
+                url: 'https://survival-eqip-limited.firebaseio.com/products/Meals Ready To Eat.json',
+            })
+            .then(function (res) {
+                $rootScope.selectedProduct = res.data;
+            })
+    }
 
-$scope.getDefult();
+    $scope.getDefult();
 
     $scope.setPrd = function (value) {
         $rootScope.selectedProduct = value;
         console.log(value);
+        $rootScope.selectedSubProduct = null;
 
     }
 
@@ -37,11 +38,11 @@ $scope.getDefult();
     }
 
 
- var ref = firebase.database().ref('clients');
- $rootScope.clients =  $firebaseArray(ref);
+    var ref = firebase.database().ref('clients');
+    $rootScope.clients = $firebaseArray(ref);
 
-  var ref = firebase.database().ref('products');
- $scope.products =  $firebaseObject(ref);
+    var ref = firebase.database().ref('products');
+    $scope.products = $firebaseObject(ref);
 
 
 
